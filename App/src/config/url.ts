957f2c7fef9761b3978c1a7ab5b271a4b67d4e01
IrfanSarang <irfansarang124@gmail.com>
2025-12-29 12:56:04 +0530

    Deployed backend

--- App/src/config/url.ts
+++ App/src/config/url.ts
@@ -1,5 +1,5 @@
 const BASE_URL = __DEV__
   ? 'http://192.168.0.102:5000' // local dev server
-  : 'https://ai-nutritionist-5jyf.onrender.com';
+  : 'https://ai-nutritionist-5axb.onrender.com'; // production server
 
 export default BASE_URL;
